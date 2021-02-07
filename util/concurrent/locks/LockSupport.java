9c4907f0dc9741fb4978801e7d455fe4485ce201
zhucj <zhucj@servyou.com.cn>
2021-02-07 10:20:47 +0800

    LockSupport

--- util/concurrent/locks/LockSupport.java
+++ util/concurrent/locks/LockSupport.java
@@ -1,40 +1,4 @@
-/*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
-/*
- * This file is available under and governed by the GNU General Public
- * License version 2 only, as published by the Free Software Foundation.
- * However, the following notice accompanied the original version of this
- * file:
- *
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
-
 package java.util.concurrent.locks;
-import sun.misc.Unsafe;
 
 /**
  * Basic thread blocking primitives for creating locks and other
@@ -77,7 +41,7 @@ import sun.misc.Unsafe;
  * useful for most concurrency control applications.  The {@code park}
  * method is designed for use only in constructions of the form:
  *
- *  <pre> {@code
+ * <pre> {@code
  * while (!canProceed()) { ... LockSupport.park(this); }}</pre>
  *
  * where neither {@code canProceed} nor any other actions prior to the
@@ -87,7 +51,7 @@ import sun.misc.Unsafe;
  *
  * <p><b>Sample Usage.</b> Here is a sketch of a first-in-first-out
  * non-reentrant lock class:
- *  <pre> {@code
+ * <pre> {@code
  * class FIFOMutex {
  *   private final AtomicBoolean locked = new AtomicBoolean(false);
  *   private final Queue<Thread> waiters
@@ -117,11 +81,15 @@ import sun.misc.Unsafe;
  *   }
  * }}</pre>
  */
+@SuppressWarnings("all")
 public class LockSupport {
-    private LockSupport() {} // Cannot be instantiated.
+
+    private LockSupport() {
+    } // Cannot be instantiated.
 
     private static void setBlocker(Thread t, Object arg) {
         // Even though volatile, hotspot doesn't need a write barrier here.
+        //即使易变，热点在这里也不需要写障碍。
         UNSAFE.putObject(t, parkBlockerOffset, arg);
     }
 
@@ -134,11 +102,12 @@ public class LockSupport {
      * thread has not been started.
      *
      * @param thread the thread to unpark, or {@code null}, in which case
-     *        this operation has no effect
+     * this operation has no effect
      */
     public static void unpark(Thread thread) {
-        if (thread != null)
+        if (thread != null) {
             UNSAFE.unpark(thread);
+        }
     }
 
     /**
@@ -166,7 +135,7 @@ public class LockSupport {
      * for example, the interrupt status of the thread upon return.
      *
      * @param blocker the synchronization object responsible for this
-     *        thread parking
+     * thread parking
      * @since 1.6
      */
     public static void park(Object blocker) {
@@ -204,7 +173,7 @@ public class LockSupport {
      * upon return.
      *
      * @param blocker the synchronization object responsible for this
-     *        thread parking
+     * thread parking
      * @param nanos the maximum number of nanoseconds to wait
      * @since 1.6
      */
@@ -245,9 +214,9 @@ public class LockSupport {
      * upon return.
      *
      * @param blocker the synchronization object responsible for this
-     *        thread parking
+     * thread parking
      * @param deadline the absolute time, in milliseconds from the Epoch,
-     *        to wait until
+     * to wait until
      * @since 1.6
      */
     public static void parkUntil(Object blocker, long deadline) {
@@ -270,8 +239,9 @@ public class LockSupport {
      * @since 1.6
      */
     public static Object getBlocker(Thread t) {
-        if (t == null)
+        if (t == null) {
             throw new NullPointerException();
+        }
         return UNSAFE.getObjectVolatile(t, parkBlockerOffset);
     }
 
@@ -334,8 +304,9 @@ public class LockSupport {
      * @param nanos the maximum number of nanoseconds to wait
      */
     public static void parkNanos(long nanos) {
-        if (nanos > 0)
+        if (nanos > 0) {
             UNSAFE.park(false, nanos);
+        }
     }
 
     /**
@@ -366,7 +337,7 @@ public class LockSupport {
      * upon return.
      *
      * @param deadline the absolute time, in milliseconds from the Epoch,
-     *        to wait until
+     * to wait until
      */
     public static void parkUntil(long deadline) {
         UNSAFE.park(true, deadline);
@@ -383,32 +354,39 @@ public class LockSupport {
             r ^= r << 13;   // xorshift
             r ^= r >>> 17;
             r ^= r << 5;
-        }
-        else if ((r = java.util.concurrent.ThreadLocalRandom.current().nextInt()) == 0)
+        } else if ((r = java.util.concurrent.ThreadLocalRandom.current().nextInt()) == 0) {
             r = 1; // avoid zero
+        }
         UNSAFE.putInt(t, SECONDARY, r);
         return r;
     }
 
     // Hotspot implementation via intrinsics API
     private static final sun.misc.Unsafe UNSAFE;
+
     private static final long parkBlockerOffset;
+
     private static final long SEED;
+
     private static final long PROBE;
+
     private static final long SECONDARY;
+
     static {
         try {
             UNSAFE = sun.misc.Unsafe.getUnsafe();
             Class<?> tk = Thread.class;
             parkBlockerOffset = UNSAFE.objectFieldOffset
-                (tk.getDeclaredField("parkBlocker"));
+                    (tk.getDeclaredField("parkBlocker"));
             SEED = UNSAFE.objectFieldOffset
-                (tk.getDeclaredField("threadLocalRandomSeed"));
+                    (tk.getDeclaredField("threadLocalRandomSeed"));
             PROBE = UNSAFE.objectFieldOffset
-                (tk.getDeclaredField("threadLocalRandomProbe"));
+                    (tk.getDeclaredField("threadLocalRandomProbe"));
             SECONDARY = UNSAFE.objectFieldOffset
-                (tk.getDeclaredField("threadLocalRandomSecondarySeed"));
-        } catch (Exception ex) { throw new Error(ex); }
+                    (tk.getDeclaredField("threadLocalRandomSecondarySeed"));
+        } catch (Exception ex) {
+            throw new Error(ex);
+        }
     }
 
 }
